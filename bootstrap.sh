59ad70b03c005a96a78c69ebae86d9714c04e9a4
A. Bram Neijt <bneijt@gmail.com>
2013-11-05 20:56:27 +0100

    Move credentials template into bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,14 @@ set -e
 if [ ! -d "virtualenv" ]; then
     virtualenv -p `which python2` virtualenv
 fi
-
+if [ ! -f credentials.py ]; then
+cat > credentials.py <<EOF
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+EOF
+fi
 . virtualenv/bin/activate
 
 pip install tweepy
--- credentials.py
+++
@@ -1,6 +0,0 @@
-
-
-consumer_key = None
-consumer_secret = None
-access_token = None
-access_token_secret = None
